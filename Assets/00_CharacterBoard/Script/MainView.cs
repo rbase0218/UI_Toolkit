6bb704ce130e83bf4ba44d6596b75631b07cae0e
rbase0218 <rbase@naver.com>
2023-05-15 16:30:47 +0900

    Feat : UI Document List 생성 Script 작성

--- Assets/00_CharacterBoard/Script/MainView.cs
+++ Assets/00_CharacterBoard/Script/MainView.cs
@@ -1,7 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MainView : MonoBehaviour
 {
+    [SerializeField] private VisualTreeAsset ListEntryTemplate;
+
+    private void OnEnable()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+
+        var characterListController = new CharacterListController();
+        characterListController.InitializeCharacterList(uiDocument.rootVisualElement, ListEntryTemplate);
+    }
 }
